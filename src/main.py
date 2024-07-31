4d9dedaa33a0fd96d709a96600e20147861704df
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-07-31 18:39:07 +0700

    refactored /register

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from typing import Annotated, Optional
+from contextlib import asynccontextmanager
+from typing import Annotated
 
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer
+from pymongo.errors import DuplicateKeyError
 from starlette import status
 from starlette.responses import Response
 
@@ -9,7 +11,15 @@ from db import user_collection
 from schemas import RegistrationFormSchema, LoginFormSchema, UserSchema
 from security import get_hashed_password
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    user_collection.create_index("email", unique=True)
+    user_collection.create_index("username", unique=True)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 Token = Annotated[str, Depends(oauth2_scheme)]
@@ -17,29 +27,18 @@ Token = Annotated[str, Depends(oauth2_scheme)]
 
 @app.post("/register")
 async def register(form: RegistrationFormSchema):
-    success_response = Response(status_code=status.HTTP_200_OK)
-
-    found_by_username: Optional[dict] = await user_collection.find_one({
-        "username": form.username
-    })
-    if found_by_username is not None:
-        return success_response
-
-    found_by_email: Optional[dict] = await user_collection.find_one({
-        "email": form.email
-    })
-    if found_by_email is not None:
-        return success_response
-
     user = UserSchema(
         username=form.username,
         email=form.email,
         password_hash=get_hashed_password(form.password),
     )
 
-    await user_collection.insert_one(user.model_dump(exclude={"id"}))
+    try:
+        await user_collection.insert_one(user.model_dump(exclude={"id"}))
+    except DuplicateKeyError:
+        pass
 
-    return success_response
+    return Response(status_code=status.HTTP_200_OK)
 
 
 @app.post("/login")
