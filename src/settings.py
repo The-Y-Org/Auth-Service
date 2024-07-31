fb5ed2f1378ca909c247d3f8f246fa298968231f
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-07-31 12:51:57 +0700

    Renamed MONGO_ADDRESS variables

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     jwt_secret_key: str
     jwt_algorithm: str
-    mongo_address: str
+    mongo_host: str
     mongo_username: str
     mongo_password: str
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
